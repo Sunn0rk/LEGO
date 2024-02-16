0377d96220c240415d2afdc6ec7c5527ed2e69c4
DESKTOP-3QAJ0SA\belen <belenkov_kirikll@mail.ru>
2024-02-16 20:33:52 +0300

    Privet2

--- cmd/app/constains.go
+++ cmd/app/constains.go
@@ -1,6 +1,14 @@
 package main
 
 const (
-	TOKEN_NAME_IN_OS      = "Lego_database_bot"
+	tablename  = "main_set"
+	server     = "localhost"
+	database   = "postgres"
+	port       = "5432"
+	username   = "postgres"
+	password   = "9201"
+	driverName = "postgres"
+
+	TOKEN_NAME_IN_OS = "Lego_database_bot"
 	UPDATE_CONFIG_TIMEOUT = 60
 )
